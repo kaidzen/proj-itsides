ac7a51b74c64f9dfb50c571c290d79c8803325aa
Olena Hrechykhina <117777710+Elena-Hrechyk@users.noreply.github.com>
2023-05-12 22:22:48 -0700

    add selected category

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import './helpers/get-bestsellers';
 import './categories-list';
 import './add-books-for-main-page';
+import './helpers/get-selected-category';
 import './helpers/last-word-color';
 
 import './helpers/aside-support';
