43fc06a31d03754fb98a039e8baae8b14731d28a
Oleksander-Kozlov <118023905+Oleksander-Kozlov@users.noreply.github.com>
2023-05-15 00:04:01 +0300

    Revert "add backend for authorization"

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,3 @@
-
-import './helpers/sign-up-modal';
-import './helpers/sign-in-modal';
-
-import './helpers/modal-authorization';
-
-
 import './categories-list';
 import './add-books-for-main-page';
 import './helpers/last-word-color';
@@ -27,7 +20,6 @@ import './helpers/aside-support';
 import './helpers/last-word-color';
 import './categories-list';
 
-
 import './categories-list';
 import './add-books-for-main-page';
 import './helpers/last-word-color';
@@ -37,4 +29,3 @@ import './helpers/aside-support';
 import './helpers/last-word-color';
 import './categories-list';
 import './helpers/footer';
-
