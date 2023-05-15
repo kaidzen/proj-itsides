9791b9aced2f0e3d3901c0b037ad92c744aeadd0
YaroslavaKlymenko <klymenko0105@gmail.com>
2023-05-15 21:21:22 +0200

    add header btns

--- src/js/helpers/sign-up-modal.js
+++ src/js/helpers/sign-up-modal.js
@@ -12,7 +12,6 @@ const logInBtn = document.querySelector('.header-sign-btn');
 const backdrop = document.querySelector('.authorization__bacdrop');
 const headerNav = document.querySelector('.header-nav');
 
-
 headerNav.classList.add('is-hidden');
 logInBtn.classList.remove('visually-hidden');
 
