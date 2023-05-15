3b71ade79eda71b469689416603f15e04668116a
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-05-16 00:54:23 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,32 +5,10 @@ import './helpers/get-bestsellers';
 import './helpers/get-selected-category';
 import './helpers/aside-support';
 import './helpers/header';
-import './helpers/last-word-color';
-import './categories-list';
 import './helpers/sign-up-modal';
 import './helpers/sign-in-modal';
-
 import './helpers/modal-authorization';
 // document.documentElement.setAttribute('theme', 'dark');
-
-import './categories-list';
-import './add-books-for-main-page';
-
-import './helpers/last-word-color';
-
-import './helpers/aside-support';
-
-import './helpers/last-word-color';
-import './categories-list';
-
-import './categories-list';
-import './add-books-for-main-page';
-import './helpers/last-word-color';
-
-import './helpers/aside-support';
-
-import './helpers/last-word-color';
-import './categories-list';
 import './helpers/footer';
 import './helpers/fund-array';
 import './nav-menu';
