e1fe23a7eb7865fe6364045b82969d77c85c479c
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-05-16 18:06:00 +0300

    Update markup-shop-list.js

--- src/js/helpers/markup-shop-list.js
+++ src/js/helpers/markup-shop-list.js
@@ -32,9 +32,9 @@ export const createMarkupShoppingList = arr =>
                     <div class="autor-link-shoppingList">
                         <p class="shop-book-autor">${author}</p>
                         <ul class="links-shoppingList">
-                        <li class="shop-book-link amazon-link"><a class="shop-a" href="${amazonUrl}"><img class="amazon" src="./img/icon-book-store/amazon.png" alt="${amazon} "></a></li>
-                        <li class="shop-book-link apple-store-link"><a class="shop-a" href="${appleUrl}"><img class="apple-store" src="./img/icon-book-store/apple-store.png" alt="${apple} "></a></li>
-                        <li class="shop-book-link book-shop-link"><a class="shop-a" href="${bookUrl}"><img class="book-shop" src="./img/icon-book-store/book-shop.png" alt="${book} "></a></li>
+                        <li class="shop-book-link amazon-link"><a class="shop-a" href="${amazonUrl}"><img class="amazon" src="/img/icon-book-store/amazon.png" alt="${amazon} "></a></li>
+                        <li class="shop-book-link apple-store-link"><a class="shop-a" href="${appleUrl}"><img class="apple-store" src="/img/icon-book-store/apple-store.png" alt="${apple} "></a></li>
+                        <li class="shop-book-link book-shop-link"><a class="shop-a" href="${bookUrl}"><img class="book-shop" src="/img/icon-book-store/book-shop.png" alt="${book} "></a></li>
                         </ul>
                     </div> 
                     </div>
