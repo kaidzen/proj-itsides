0be1ce87405c538743f7bb8764af3662cffbdce4
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-05-16 20:51:51 +0300

    Update fund-array.js

--- src/js/helpers/fund-array.js
+++ src/js/helpers/fund-array.js
@@ -1,58 +1,77 @@
 import fund1 from './../../img/support/fund1@1x.png';
+import fund1x2 from './../../img/support/fund1@2x.png';
+import fund2 from './../../img/support/fund2@1x.png';
+import fund2x2 from './../../img/support/fund2@2x.png';
+import fund3 from './../../img/support/fund3@1x.png';
+import fund3x2 from './../../img/support/fund3@2x.png';
+import fund4 from './../../img/support/fund4@1x.png';
+import fund4x2 from './../../img/support/fund4@2x.png';
+import fund5 from './../../img/support/fund5@1x.png';
+import fund5x2 from './../../img/support/fund5@2x.png';
+import fund6 from './../../img/support/fund6@1x.png';
+import fund6x2 from './../../img/support/fund6@2x.png';
+import fund7 from './../../img/support/fund7@1x.png';
+import fund7x2 from './../../img/support/fund7@2x.png';
+import fund8 from './../../img/support/fund8@1x.png';
+import fund8x2 from './../../img/support/fund8@2x.png';
+import fund9 from './../../img/support/fund9@1x.png';
+import fund9x2 from './../../img/support/fund9@2x.png';
+
+
 console.log(fund1);
 export const fundsArray = [
   {
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
     img: fund1,
-    img2: '.././img/support/fund1@2x.png',
+    img2: fund1x2,
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: '.././img/support/fund2@1x.png',
-    img2: '.././img/support/fund2@2x.png',
+    img: fund2,
+    img2: fund2x2,
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: '.././img/support/fund3@1x.png',
-    img2: '.././img/support/fund3@2x.png',
+    img: fund3,
+    img2: fund3x2,
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: '.././img/support/fund4@1x.png',
-    img2: '.././img/support/fund4@2x.png',
+    img: fund4,
+    img2: fund4x2,
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: '.././img/support/fund5@1x.png',
-    img2: '.././img/support/fund5@2x.png',
+    img: fund5,
+    img2: fund5x2,
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: '.././img/support/fund6@1x.png',
-    img2: '.././img/support/fund6@2x.png',
+    img: fund6,
+    img2: fund6x2,
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: '.././img/support/fund7@1x.png',
-    img2: '.././img/support/fund7@2x.png',
+    img: fund7,
+    img2: fund7x2,
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: '.././img/support/fund8@1x.png',
-    img2: '.././img/support/fund8@2x.png',
+    img: fund8,
+    img2: fund8x2,
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: '.././img/support/fund9@1x.png',
-    img2: '.././img/support/fund9@2x.png',
+    img: fund9,
+    img2: fund9x2,
   },
 ];
